9e8cda27aa4d7cb03b10e7ed021f449d67616670
musthafa-s <rodanmusthafa12@gmail.com>
2025-04-02 15:56:45 +0530

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
+import App6 from './L6Task6/App6'
 
-import App1 from '../../Level 5/App1'
+
 //import './index.css'
 //import App from '../../ToDoTask/App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
-    <App1 />
+    <App6 />
+    
   </StrictMode>,
 )
